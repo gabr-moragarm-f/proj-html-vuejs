9a63e33bc6caab6b552deccbe250876d3c1354de
Gabriel Moraga <agarom97@gmail.com>
2021-02-23 16:51:54 +0100

    js completato

--- js/script.js
+++ js/script.js
@@ -1,39 +1,56 @@
 var app = new Vue({
   el: '#root',
   data:{
-    headerMenuData:[
-      {
-        name: 'home',
-        subMenu: [],
-        new: false
+    header:{
+      menuData:[
+        {
+          name: 'home',
+          subMenu: [],
+          new: false
+        },
+        {
+          name: 'about',
+          subMenu: [],
+          new: false
+        },
+        {
+          name: 'prices',
+          subMenu: [],
+          new: false
+        },
+        {
+          name: 'courses',
+          subMenu: [],
+          new: true
+        },
+        {
+          name: 'locations',
+          subMenu: [],
+          new: false
+        },
+        {
+          name: 'blog',
+          subMenu: [],
+          new: false
+        }
+      ],
+      selectedId: 0,
+    },
+    form:{
+      inputs:{
+        name: '',
+        email: '',
+        telephone: '',
+        location: '',
       },
-      {
-        name: 'about',
-        subMenu: [],
-        new: false
+      style:{
+        name: '',
+        email: '',
+        telephone: ''
       },
-      {
-        name: 'prices',
-        subMenu: [],
-        new: false
-      },
-      {
-        name: 'courses',
-        subMenu: [],
-        new: true
-      },
-      {
-        name: 'locations',
-        subMenu: [],
-        new: false
-      },
-      {
-        name: 'blog',
-        subMenu: [],
-        new: false
-      }
-    ],
-    percentageData:[
+      bookings: [],
+    },
+    progressRingData:[
       {
         name: 'pass rate',
         percentage: '95'
@@ -47,16 +64,108 @@ var app = new Vue({
         percentage: '0'
       },
     ],
-    selectedHeaderNavId: 0,
-    userBookingName: '',
-    userBookingEmail: '',
-    userBookingTelephone: '',
-    userBookingLocation: '',
-    bookings: [],
-    inputNameStyle: '',
-    inputEmailStyle: '',
-    inputTelephoneStyle: '',
-    inputLocationStyle: ''
+    slider:{
+      testimonials: [
+        {
+          name: 'Sophia Vergara',
+          photo: 'testimonial-sophia.png',
+          statement: 'Avada Driving School really helped build my confidence behind the wheel and with driving in general, and they got me a first time pass! Highly recommended.',
+        },
+        {
+          name: 'Harold Saxon',
+          photo: 'testimonial-harold.png',
+          statement: 'Avada Driving School really helped build my confidence behind the wheel and with driving in general, and they got me a first time pass! Highly recommended.',
+        },
+        {
+          name: 'Alan Grant',
+          photo: 'testimonial-grant.png',
+          statement: 'Avada Driving School really helped build my confidence behind the wheel and with driving in general, and they got me a first time pass! Highly recommended.',
+        },
+        {
+          name: 'Kate Blanchett',
+          photo: 'testimonial-kate.png',
+          statement: 'Avada Driving School really helped build my confidence behind the wheel and with driving in general, and they got me a first time pass! Highly recommended.',
+        },
+        {
+          name: 'Kelly Shaddock (087)',
+          photo: 'testimonial-kelly.png',
+          statement: 'Avada Driving School really helped build my confidence behind the wheel and with driving in general, and they got me a first time pass! Highly recommended.',
+        }
+      ],
+      index: 0,
+      intervalSeconds: 4
+    },
+    news: [
+      {
+        title: 'What Car to Start With?',
+        image: 'blog-choosecar-700x441.jpg',
+        date: 'February 7th, 2019',
+        comments: [],
+        preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+      },
+      {
+        title: 'Avada Driving School Expanding',
+        image: 'blogpost-10and2-700x441.jpg',
+        date: 'February 7th, 2019',
+        comments: [],
+        preview: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+      }
+    ],
+    footerData:[
+      {
+        title: 'contact details',
+        subheadings: [
+          {
+            icon: 'fas fa-home',
+            content: '12345 North Main Street, New York, NY 555555'
+          },
+          {
+            icon: 'fas fa-phone',
+            content:'1.800.555.6789'
+          },
+          {
+            icon: 'far fa-envelope',
+            content:'info@your-domain.com'
+          },
+          {
+            icon: 'fas fa-link',
+            content:'Theme-Fusion.com'
+          }
+        ]
+      },
+      {
+        title: 'courses',
+        subheadings: [
+          {
+            icon: 'far fa-arrow-alt-circle-right',
+            content:'Pass Plus'
+          },
+          {
+            icon: 'far fa-arrow-alt-circle-right',
+            content:'Intensive Course'
+          },
+          {
+            icon: 'far fa-arrow-alt-circle-right',
+            content:'Automatic'
+          },
+          {
+            icon: 'far fa-arrow-alt-circle-right',
+            content:'Instructor Training'
+          }
+        ]
+      }
+    ]
+  },
+  mounted() {
+    let that = this;
+
+    setInterval(function () {
+      if (that.slider.index >= (that.slider.testimonials.length - 1)) {
+        that.slider.index = 0;
+      }else {
+        that.slider.index++;
+      }
+    }, (this.slider.intervalSeconds * 1000));
   },
   methods:{
     hasNumber:function(string) {
@@ -65,31 +174,43 @@ var app = new Vue({
     formValidation: function() {
       let validationFlag = true;
 
-      if (this.userBookingName === '') {
-        this.inputNameStyle = 'required';
+      if (this.form.inputs.name === '') {
+        this.form.style.name = 'required';
 
         validationFlag = false;
       }
 
-      if (this.hasNumber(this.userBookingName)) {
-        this.inputNameStyle = 'required';
+      if (this.hasNumber(this.form.inputs.name)) {
+        this.form.style.name = 'required';
 
         validationFlag = false;
       }
 
-      if (this.userBookingEmail === '') {
-        this.inputNameStyle = 'required';
+      if (this.form.inputs.email === '') {
+        this.form.style.name = 'required';
 
         validationFlag = false;
       }
 
-      if (!this.userBookingEmail.includes('@')) {
-        this.inputEmailStyle = 'required';
+      if (!this.form.inputs.email.includes('@')) {
+        this.form.style.email = 'required';
+
+        validationFlag = false;
+      }
+
+      if (isNaN(this.form.inputs.telephone) && this.form.inputs.telephone !== '') {
+        this.form.style.telephone = 'required';
 
         validationFlag = false;
       }
 
       if (validationFlag) {
+        this.form.style.name = '';
+
+        this.form.style.email = '';
+
+        this.form.style.telephone = '';
+
         return true;
       }
 
@@ -97,16 +218,30 @@ var app = new Vue({
     },
     sendBooking: function() {
       if (this.formValidation()) {
-        return;
+        this.form.bookings.push({
+          name: this.form.inputs.name,
+          email: this.form.inputs.email,
+          telephone: this.form.inputs.telephone,
+          location: this.form.inputs.location
+        });
+
+        alert('Book done')
+      }else{
+        alert('Check the entered data');
       }
 
-      this.bookings.push({
-        name: this.userBookingName,
-        email: this.userBookingEmail,
-        telephone: this.userBookingTelephone,
-        location: this.userBookingLocation
-      });
     },
+    isIconFull: function(index) {
+      if (index === this.slider.index) {
+        return 'fas';
+      }
+
+      return 'far';
+    },
+    returnTop: function() {
+      window.scrollTop = 0;
+      alert()
+    }
   }
 });
 
